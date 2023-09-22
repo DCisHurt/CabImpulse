37a153713bdb386950e4741e8b2ef751b33e9db7
DCisHurt <roger33669@gmail.com>
2023-09-22 23:40:41 +0100

    add signal chain

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -3,14 +3,14 @@
 
 //==============================================================================
 CabImpulseAudioProcessor::CabImpulseAudioProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+    : AudioProcessor(BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+                         .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+                         .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+      )
 {
 }
 
@@ -26,29 +26,29 @@ const juce::String CabImpulseAudioProcessor::getName() const
 
 bool CabImpulseAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool CabImpulseAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool CabImpulseAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double CabImpulseAudioProcessor::getTailLengthSeconds() const
@@ -58,8 +58,8 @@ double CabImpulseAudioProcessor::getTailLengthSeconds() const
 
 int CabImpulseAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1; // NB: some hosts don't cope very well if you tell them there are 0 programs,
+              // so this should be at least 1, even if you're not really implementing programs.
 }
 
 int CabImpulseAudioProcessor::getCurrentProgram()
@@ -67,28 +67,37 @@ int CabImpulseAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void CabImpulseAudioProcessor::setCurrentProgram (int index)
+void CabImpulseAudioProcessor::setCurrentProgram(int index)
 {
-    juce::ignoreUnused (index);
+    juce::ignoreUnused(index);
 }
 
-const juce::String CabImpulseAudioProcessor::getProgramName (int index)
+const juce::String CabImpulseAudioProcessor::getProgramName(int index)
 {
-    juce::ignoreUnused (index);
+    juce::ignoreUnused(index);
     return {};
 }
 
-void CabImpulseAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void CabImpulseAudioProcessor::changeProgramName(int index, const juce::String &newName)
 {
-    juce::ignoreUnused (index, newName);
+    juce::ignoreUnused(index, newName);
 }
 
 //==============================================================================
-void CabImpulseAudioProcessor::prepareToPlay (double sCableRate, int sCablesPerBlock)
+void CabImpulseAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-    juce::ignoreUnused (sCableRate, sCablesPerBlock);
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = 1;
+
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
+
+    auto chainSettings = getChainSettings(apvts);
+
+    updateCutFilter(chainSettings);
+    updateImpulseResponse(chainSettings);
 }
 
 void CabImpulseAudioProcessor::releaseResources()
@@ -97,37 +106,36 @@ void CabImpulseAudioProcessor::releaseResources()
     // spare memory, etc.
 }
 
-bool CabImpulseAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool CabImpulseAudioProcessor::isBusesLayoutSupported(const BusesLayout &layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+        // This checks if the input layout matches the output layout
+#if !JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 
-void CabImpulseAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages)
+void CabImpulseAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer,
+                                            juce::MidiBuffer &midiMessages)
 {
-    juce::ignoreUnused (midiMessages);
+    juce::ignoreUnused(midiMessages);
 
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
     // In case we have more outputs than inputs, this code clears any output
@@ -137,20 +145,23 @@ void CabImpulseAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // when they first compile a plugin, but obviously you don't need to keep
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+        buffer.clear(i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the sCables and the outer loop is handling the channels.
-    // Alternatively, you can process the sCables with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
-    }
+    auto chainSettings = getChainSettings(apvts);
+
+    updateCutFilter(chainSettings);
+    updateImpulseResponse(chainSettings);
+
+    juce::dsp::AudioBlock<float> block(buffer);
+
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
 }
 
 //==============================================================================
@@ -159,39 +170,150 @@ bool CabImpulseAudioProcessor::hasEditor() const
     return true; // (change this to false if you choose to not supply an editor)
 }
 
-juce::AudioProcessorEditor* CabImpulseAudioProcessor::createEditor()
+juce::AudioProcessorEditor *CabImpulseAudioProcessor::createEditor()
 {
     // return new CabImpulseAudioProcessorEditor (*this);
     return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void CabImpulseAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void CabImpulseAudioProcessor::getStateInformation(juce::MemoryBlock &destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    juce::ignoreUnused(destData);
 }
 
-void CabImpulseAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void CabImpulseAudioProcessor::setStateInformation(const void *data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+    juce::ignoreUnused(data, sizeInBytes);
 }
 
+void CabImpulseAudioProcessor::updateCutFilter(const ChainSettings &chainSettings)
+{
+    // int filterOrder = 2;
+
+    auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq,
+                                                                                                          getSampleRate(),
+                                                                                                          4);
+
+    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq,
+                                                                                                          getSampleRate(),
+                                                                                                          4);
+
+    auto &leftLowCut = leftChain.get<ChainPositions::LowCut>();
+    auto &rightLowCut = rightChain.get<ChainPositions::LowCut>();
+    auto &leftHighCut = leftChain.get<ChainPositions::HighCut>();
+    auto &rightHighCut = rightChain.get<ChainPositions::HighCut>();
+
+    *leftLowCut.get<0>().coefficients = *lowCutCoefficients[0];
+    *leftLowCut.get<1>().coefficients = *lowCutCoefficients[1];
+    *rightLowCut.get<0>().coefficients = *lowCutCoefficients[0];
+    *rightLowCut.get<1>().coefficients = *lowCutCoefficients[1];
+
+    *leftHighCut.get<0>().coefficients = *highCutCoefficients[0];
+    *leftHighCut.get<1>().coefficients = *highCutCoefficients[1];
+    *rightHighCut.get<0>().coefficients = *highCutCoefficients[0];
+    *rightHighCut.get<1>().coefficients = *highCutCoefficients[1];
+}
+
+void CabImpulseAudioProcessor::updateImpulseResponse(const ChainSettings &chainSettings)
+{
+
+    const char *binaryData = 0;
+    int binaryDataSize = 0;
+
+    binaryData = BinaryData::getNamedResource(BinaryData::namedResourceList[0], binaryDataSize);
+
+    auto *inputStream = new MemoryInputStream(binaryData, binaryDataSize, false);
+    WavAudioFormat format;
+    auto reader = format.createReaderFor(inputStream, true);
+
+    // If reader was successfully created, load into AudioSampleBuffer array
+    if (reader)
+    {
+        AudioBuffer<float> buffer(static_cast<int>(reader->numChannels),
+                                  static_cast<int>(reader->lengthInSamples));
+        reader->read(buffer.getArrayOfWritePointers(), buffer.getNumChannels(), 0, buffer.getNumSamples());
+
+        leftChain.get<ChainPositions::IRloader>().loadImpulseResponse(std::move(buffer),
+                                                                      getSampleRate(),
+                                                                      juce::dsp::Convolution::Stereo::no,
+                                                                      juce::dsp::Convolution::Trim::yes,
+                                                                      juce::dsp::Convolution::Normalise::yes);
+
+        rightChain.get<ChainPositions::IRloader>().loadImpulseResponse(std::move(buffer),
+                                                                       getSampleRate(),
+                                                                       juce::dsp::Convolution::Stereo::no,
+                                                                       juce::dsp::Convolution::Trim::yes,
+                                                                       juce::dsp::Convolution::Normalise::yes);
+    }
+    else
+    {
+        jassertfalse;
+        return;
+    }
+}
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState &apvts)
+{
+    ChainSettings settings;
+
+    settings.lowCutFreq = apvts.getRawParameterValue("Low Cut")->load();
+    settings.highCutFreq = apvts.getRawParameterValue("High Cut")->load();
+    settings.micType = (int)apvts.getRawParameterValue("Mic Type")->load();
+    settings.micDistance = (int)apvts.getRawParameterValue("Mic Distance")->load();
+    settings.micOffAxis = apvts.getParameter("Mic Off Axis")->getValue();
+
+    return settings;
+}
 
 juce::AudioProcessorValueTreeState::ParameterLayout CabImpulseAudioProcessor::createParameterLayout()
 {
     juce::AudioProcessorValueTreeState::ParameterLayout layout;
-    layout.add(std::make_unique<juce::AudioParameterFloat>("gain", "Gain", 0.0f, 1.0f, 0.5f));
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Gain",
+                                                           "Gain",
+                                                           juce::NormalisableRange<float>(-60.0f, 6.0f, 0.1f, 1.0f),
+                                                           1.0f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Low Cut",
+                                                           "Low Cut",
+                                                           juce::NormalisableRange<float>(20.0f, 20000.0f, 1.0f, 1.0f),
+                                                           20.0f));
+
+    layout.add(std::make_unique<juce::AudioParameterFloat>("High Cut",
+                                                           "High Cut",
+                                                           juce::NormalisableRange<float>(20.0f, 20000.0f, 1.0f, 1.0f),
+                                                           20000.0f));
+
+    layout.add(std::make_unique<juce::AudioParameterChoice>("Cab",
+                                                            "Cab",
+                                                            juce::StringArray{"1x12", "2x12", "4x12"},
+                                                            0));
+
+    layout.add(std::make_unique<juce::AudioParameterChoice>("Mic Type",
+                                                            "Mic Type",
+                                                            juce::StringArray{"57", "121", "409", "421", "545", "i5", "REF", "U87"},
+                                                            0));
+
+    layout.add(std::make_unique<juce::AudioParameterChoice>("Mic Distance",
+                                                            "Mic Distance",
+                                                            juce::StringArray{"Close", "Mid", "Far"},
+                                                            0));
+
+    layout.add(std::make_unique<juce::AudioParameterBool>("Mic Off Axis",
+                                                          "Mic Off Axis",
+                                                          false));
+
     return layout;
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter()
 {
     return new CabImpulseAudioProcessor();
 }
--- PluginProcessor.h
+++ PluginProcessor.h
@@ -1,11 +1,20 @@
 #pragma once
 
-#include <juce_audio_processors/juce_audio_processors.h>
-#include <juce_dsp/juce_dsp.h>
+// #include <juce_audio_processors/juce_audio_processors.h>
+// #include <juce_dsp/juce_dsp.h>
+#include <JuceHeader.h>
 
+struct ChainSettings
+{
+    float lowCutFreq{0}, highCutFreq{0};
+    int micType{0}, micDistance{0};
+    bool micOffAxis{0};
+};
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState &apvts);
 
 //==============================================================================
-class CabImpulseAudioProcessor  : public juce::AudioProcessor
+class CabImpulseAudioProcessor : public juce::AudioProcessor
 {
 public:
     //==============================================================================
@@ -13,16 +22,16 @@ public:
     ~CabImpulseAudioProcessor() override;
 
     //==============================================================================
-    void prepareToPlay (double sCableRate, int sCablesPerBlock) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
     using AudioProcessor::processBlock;
 
     //==============================================================================
-    juce::AudioProcessorEditor* createEditor() override;
+    juce::AudioProcessorEditor *createEditor() override;
     bool hasEditor() const override;
 
     //==============================================================================
@@ -36,19 +45,94 @@ public:
     //==============================================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+    void setCurrentProgram(int index) override;
+    const juce::String getProgramName(int index) override;
+    void changeProgramName(int index, const juce::String &newName) override;
 
     //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+    void getStateInformation(juce::MemoryBlock &destData) override;
+    void setStateInformation(const void *data, int sizeInBytes) override;
 
     static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
+    juce::AudioProcessorValueTreeState apvts{*this, nullptr, "Parameters", createParameterLayout()};
 
 private:
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter>;
+    using ImpulseLoader = juce::dsp::Convolution;
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, ImpulseLoader, CutFilter>;
+
+    MonoChain leftChain, rightChain;
+
+    enum ChainPositions
+    {
+        LowCut,
+        IRloader,
+        HighCut
+    };
+
+    // enum CabType
+    // {
+    //     SM57,
+    //     SM58,
+    //     MD421,
+    //     MD441,
+    //     U87,
+    //     R121,
+    //     R122,
+    //     R160,
+    //     R121Ribbon,
+    //     R122Ribbon,
+    //     R160Ribbon,
+    //     OffAxis
+    // };
+
+// Marshall 1936 2×12
+// Marshall 1960 4×12
+// Marshall 1960AHW 4×12
+// Marshall 1970 4×12
+// Line 6 Vetta 4×12
+// ENGL Pro 4×12
+// Randall RS412XLT100 4×12
+// Krank Krankenstein 4×12
+// Bogner Uberkab 4×12
+// Mesa Standard 4×12
+// Orange 4×12
+// Genz Benz G-Flex ported 2×12
+// Fender 1965 Super Reverb 4x10
+// Roland JC120 2×12
+// Supro Thunderbolt 1x15
+// Vox AC30 2×12
+// Fender Deluxe 1x12
+// Ampeg SVT 8x10
+// Ampeg Portaflex 1x15
+// Aguilar DB 4x12
+// Gallien-Krueger Neo 4x10
+
+
+
+    enum MicDistance
+    {
+        Close,
+        Near,
+        Far
+    };
+
+    enum MicType
+    {
+        SM57,
+        R121,
+        MD409,
+        MD421,
+        S545SD,
+        ADi5,
+        M30,
+        U87
+    };
+
+    void updateCutFilter(const ChainSettings &chainSettings);
+    void updateImpulseResponse(const ChainSettings &chainSettings);
 
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CabImpulseAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(CabImpulseAudioProcessor)
 };
